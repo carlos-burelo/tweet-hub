c0a757013e0e8be1cbaa1c15903acc2a474d19ff
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-06-25 13:24:58 -0500

    :sparkles: feat: add hover card in username

--- src/components/Follow/FollowCard.tsx
+++ src/components/Follow/FollowCard.tsx
@@ -1,5 +1,46 @@
+import Avatar from '#components/Avatar'
+import Button from '#components/Button'
+import { userMock } from '#mocks/user'
 import _ from './FollowCard.module.css'
 
-export default function FollowCard({}) {
-  return <div className={_.followCard}></div>
+interface Props {
+  top: boolean
+  onHide: (value: boolean) => void
+}
+export default function FollowCard({ top, onHide }: Props) {
+  return (
+    <div
+      className={_.followCard}
+      onMouseLeave={() => setTimeout(() => onHide(false), 300)}
+      onMouseEnter={() => onHide(true)}
+      style={{
+        bottom: top ? '20px' : '-220px',
+      }}
+    >
+      <div className={_.header}>
+        <div className={_.avatar}>
+          <Avatar src={userMock.avatar} size={80} />
+        </div>
+        <div className={_.btn}>
+          <Button text='Seguir' />
+        </div>
+      </div>
+      <div className={_.body}>
+        <div className={_.displayNames}>
+          <h2 className={_.name}>{userMock.name}</h2>
+          <p className={_.usernname}></p>
+        </div>
+        <p className={_.bio}>{userMock.bio}</p>
+        <div className={_.otherInfo}>
+          <div className={_.following}>
+            <span className={_.followCount}>10</span>
+            <span className={_.followText}>Siguiendo</span>
+          </div>
+          <div className={_.followers}></div>
+          <span className={_.followCount}>10</span>
+          <span className={_.followText}>Seguidores</span>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/components/UserName.tsx
+++ src/components/UserName.tsx
@@ -1,14 +1,42 @@
 import _ from './UserName.module.css'
 import Link from 'next/link'
+import FollowCard from './Follow/FollowCard'
+import { useState } from 'react'
+import useWindowSize from '#hooks/useWindowSize'
 
 interface Props {
   id: string
 }
 
 export default function UserName({ id }: Props) {
+  // muestra la información del usuario cuando se hace hover en el nombre de usuario y se ajusta a la posición  dependiendo del tamaño de la pantalla y
+  const [showInfo, setShowInfo] = useState(false)
+  const { height, width } = useWindowSize()
+  const [positionUp, setPositionUp] = useState(false)
+  const handleInfoPosition = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => {
+    const Limit = 300 + 70
+    let positionUp = false
+    const mouse = { x: e.clientX, y: e.clientY }
+    const viewport = { x: width, y: height }
+    console.log(mouse, viewport)
+    if (mouse.y + Limit > height) positionUp = true
+    setPositionUp(positionUp)
+  }
   return (
-    <Link href={`/${id}`}>
-      <a className={_.username}>@{id}</a>
-    </Link>
+    <div className={_.link}>
+      <Link href={`/${id}`}>
+        <a
+          className={_.username}
+          onMouseMove={handleInfoPosition}
+          onMouseEnter={() => setShowInfo(true)}
+          onMouseLeave={() => setShowInfo(false)}
+        >
+          @{id}
+        </a>
+      </Link>
+      {showInfo && <FollowCard top={positionUp} onHide={setShowInfo} />}
+    </div>
   )
 }
