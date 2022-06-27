0d08af95830214aee8976d20a3b1f1d0f6f17c1a
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-06-26 23:32:17 -0500

    :wrench: chore: initialize zustand context

---
+++ src/contexts/tweetStore.ts
@@ -0,0 +1,19 @@
+import create from "zustand"
+import { Tweet } from "#types"
+
+
+interface TweetStore {
+  tweets: Tweet[]
+  addTweets: (tweets: Tweet[]) => void
+  addTweet: (tweet: Tweet) => void
+  removeTweet: (id: string) => void
+}
+
+const tweetStore = create<TweetStore>((set) => ({
+  tweets: [],
+  addTweet: (tweet) => set((state) => ({ tweets: [...state.tweets, tweet] })),
+  addTweets: (tweets) => set((state) => ({ tweets: [...state.tweets, ...tweets] })),
+  removeTweet: (id) => set((state) => ({ tweets: state.tweets.filter((tweet) => tweet.id !== id) })),
+}))
+
+export default tweetStore
