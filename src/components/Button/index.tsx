9403ce6d094d47192e04c890c78bd62b3bfb9078
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-06-28 10:16:11 -0500

    :art: styles(Button): add button styles for dynamic props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,12 @@ import _ from './Button.module.scss'
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text: string
+  primary?: boolean
 }
 
-export default function Button({ text, ...props }: Props) {
+export default function Button({ text, primary, ...props }: Props) {
   return (
-    <button className={_.button} {...props}>
+    <button className={`${_.button} ${primary ? _.primary : ''}`} {...props}>
       {text}
     </button>
   )
