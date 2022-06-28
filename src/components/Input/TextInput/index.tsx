169c53452c70468c5544dd58be44e003e9b17169
carlos-burelo <carlosfernandoburelo@gmail.com>
2022-06-28 10:17:06 -0500

    :sparkles: feat: add tweet box for create a new tweet

--- src/components/Input/TextInput/index.tsx
+++ src/components/Input/TextInput/index.tsx
@@ -3,5 +3,5 @@ import _ from './TextInput.module.scss'
 interface Props extends React.InputHTMLAttributes<HTMLTextAreaElement> {}
 
 export default function TextInput({ ...props }: Props) {
-  return <textarea className={_.textarea} {...props} />
+  return <textarea className={_.textinput} {...props} />
 }
